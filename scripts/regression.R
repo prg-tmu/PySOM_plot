899ec4079a34cefb05ee6d2eb64894fb6007485c
Yusuke Izawa <me@yizawa.com>
2024-01-12 17:14:40 +0900

    update regression.R

--- scripts/regression.R
+++ scripts/regression.R
@@ -9,13 +9,25 @@ if (length(args)==0) {
   args[2] = "out.pdf"
 }
 
-# library(ggplot2)
+library(ggplot2)
 
 pdf(args[2])
 data <- read.table(args[1], head = TRUE, sep = ",")
 y <- data$count
 x <- data$rank
+# Perform power regression
 m <- lm(log10(y) ~ log10(x))
-plot(x, y, log = "xy")
-abline(m, col = "blue")
-legend("bottomleft", legend=paste("R^2 is", format(summary(m)$r.squared, digits = 3)))
+# plot(x, y, log = "xy")
+# abline(m, col = "blue")
+# legend("bottomleft", legend=paste("R^2 is", format(summary(m)$r.squared, digits = 3)))
+# Do same thing below by using ggplot2
+p <- ggplot(data, aes(x = rank, y = count)) + geom_point() +
+    geom_smooth(method = "lm", se = FALSE) +
+    geom_label(data = data,
+               aes(x = Inf, y = Inf,
+                   label = paste("R2 =", summary(m)$r.squared, sep = " ")),
+               hjust = 1, vjust = 1)
+# Log base 10 scale + log ticks (on left and bottom side)
+p + scale_y_log10() +
+    scale_x_log10() +
+    annotation_logticks(sides = "lb")
