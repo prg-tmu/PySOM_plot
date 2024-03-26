f8548b9fac47ec2544273066c483eea7007563e0
Yusuke Izawa <me@yizawa.com>
2024-03-27 00:44:26 +0900

    Update pysomplot_pypylog to show the relation between compilation time and bytecode size

--- pysomplot/pysomplot_pypylog.py
+++ pysomplot/pysomplot_pypylog.py
@@ -5,6 +5,9 @@ import re
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.linear_model import LinearRegression
 
 from base import Base
 
@@ -13,6 +16,9 @@ class PySOMPlotPyPyLog(Base):
         super().__init__(path, standardize, suffix)
 
         self.data = {}
+
+        sns.set_style("darkgrid")
+        sns.set_context("paper")
 
     def _setup_data(self, name, exe):
         if exe not in self.data:
@@ -73,14 +79,62 @@ class PySOMPlotPyPyLog(Base):
             for name in result[exe]:
                 medians[exe][name] = {}
                 variances[exe][name] = {}
-                medians[exe][name] = st.median(result[exe][name])
-                variances[exe][name] = st.variance(result[exe][name])
+                medians[exe][name] = st.median(result[exe][name]) * 1e3 # second to millisecond
+                variances[exe][name] = st.variance(result[exe][name]) * 1e3
+
+        df_bytesize = pd.read_csv("data/benchmark_bytesize.csv")
+        df_bytesize["program"] = df_bytesize["program"].str.lower()
+        df_bytesize = df_bytesize.set_index("program")
+        df_bytesize = df_bytesize.drop("json")
 
         df_med = pd.DataFrame(medians)
         df_var = pd.DataFrame(variances)
-        new_df_med = df_med["threaded"] / df_med["tracing"]
-        new_df_var = df_var["threaded"] / df_var["tracing"]
-        new_df_med.plot.bar()
+
+        df = df_bytesize.join(df_med)
+
+        x = df["size"]
+        y_threaded = df["threaded"]
+        y_tracing = df["tracing"]
+        yerr_threaded = df_var["threaded"]
+        yerr_tracing = df_var["tracing"]
+
+        fig, ax = plt.subplots()
+        ax.scatter(x, y_threaded, c='tab:blue', s=100)
+        ax.scatter(x, y_tracing, c='tab:red', s=100)
+
+        x1, x2, y1, y2 = -100, 1750, -25, 1100
+        axins = ax.inset_axes(
+            [0.42, 0.42, 0.58, 0.58],
+            xlim=(x1, x2), ylim=(y1, y2),
+        )
+
+        mod = LinearRegression()
+
+        df_x = pd.DataFrame(x).drop("pagerank")
+        df_y = pd.DataFrame(y_threaded).drop("pagerank")
+        mod_lin = mod.fit(df_x, df_y)
+        y_lin_fit = mod_lin.predict(df_x)
+        r2_lin = mod.score(df_x, df_y)
+
+        axins.scatter(x, y_threaded, c='tab:blue', s=100)
+        axins.scatter(x, y_tracing, c='tab:red', s=100)
+
+        axins.plot(df_x, y_lin_fit, color='tab:blue', linewidth=2)
+        axins.text(1250, 50, '$ y = $' + str(round(mod.coef_[0][0], 4)) + " x + " + str(round(mod.intercept_[0], 2)), fontsize=12)
+        axins.text(1250, 100, '$ R^{2} $=' + str(round(r2_lin, 4)), fontsize=12)
+
+        df_x = pd.DataFrame(x).drop("pagerank")
+        df_y = pd.DataFrame(y_tracing).drop("pagerank")
+        mod_lin = mod.fit(df_x, df_y)
+        y_lin_fit = mod_lin.predict(df_x)
+        r2_lin = mod.score(df_x, df_y)
+
+        axins.plot(df_x, y_lin_fit, color='tab:red', linewidth=2)
+        axins.text(100, 300, '$ y = $' + str(round(mod.coef_[0][0], 4)) + " x + " + str(round(mod.intercept_[0], 2)), fontsize=12)
+        axins.text(100, 350, '$ R^{2} $=' + str(round(r2_lin, 4)), fontsize=12)
+
+        ax.indicate_inset_zoom(axins, edgecolor="black")
+
         plt.show()
 
 
